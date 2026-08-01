11d758d246cbd1d259f3c5f20e4a666962ca047e
Cristian-Tavarez <cristian22041@gmail.com>
2026-08-01 14:00:40 -0700

    Actualiza el AppModule.kt con el AppDatabase

--- app/src/main/java/com/example/micardioat/di/AppModule.kt
+++ app/src/main/java/com/example/micardioat/di/AppModule.kt
@@ -1,16 +1,37 @@
 package com.example.micardioat.di
 
+import android.content.Context
+import androidx.room.Room
+import com.example.micardioat.data.dao.UsuarioDao
+import com.example.micardioat.data.database.AppDatabase
 import com.example.micardioat.data.repository.PacienteCardiologiaRepository
 import com.example.micardioat.domain.use_case.*
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "micardioat_db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideUsuarioDao(appDatabase: AppDatabase): UsuarioDao {
+        return appDatabase.usuarioDao()
+    }
 
     @Provides
     @Singleton
@@ -44,13 +65,13 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideLoginUseCase(): LoginUseCase {
-        return LoginUseCase()
+    fun provideLoginUseCase(usuarioDao: UsuarioDao): LoginUseCase {
+        return LoginUseCase(usuarioDao)
     }
 
     @Provides
     @Singleton
-    fun provideRegisterUseCase(): RegisterUseCase {
-        return RegisterUseCase()
+    fun provideRegisterUseCase(usuarioDao: UsuarioDao): RegisterUseCase {
+        return RegisterUseCase(usuarioDao)
     }
 }
