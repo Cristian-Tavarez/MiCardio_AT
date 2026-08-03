88dcd449cd6b0d57c8cf807ac7c39f8c375eedd0
Heyson Polanco <Heysonpolanco65@gmail.com>
2026-08-03 17:16:15 -0400

    added paciente classes to the databsae

--- app/src/main/java/com/example/micardioat/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/micardioat/data/database/AppDatabase.kt
@@ -2,10 +2,18 @@ package com.example.micardioat.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.micardioat.data.dao.PacienteDao
 import com.example.micardioat.data.dao.UsuarioDao
+import com.example.micardioat.data.entity.PacienteEntity
 import com.example.micardioat.data.entity.UsuarioEntity
 
-@Database(entities = [UsuarioEntity::class], version = 1, exportSchema = false)
+@Database(
+    entities = [UsuarioEntity::class, PacienteEntity::class],
+    version = 1,
+    exportSchema = false
+)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun usuarioDao(): UsuarioDao
+
+    abstract fun pacienteDao(): PacienteDao
 }
