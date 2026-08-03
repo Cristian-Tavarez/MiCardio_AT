c2182130f2c4665614c4b0b22543c4f4c7fc6b03
Heyson Polanco <Heysonpolanco65@gmail.com>
2026-08-03 18:11:39 -0400

    Update list ViewModel to collect Room flows

--- app/src/main/java/com/example/micardioat/presentation/paciente_list/PacienteListViewModel.kt
+++ app/src/main/java/com/example/micardioat/presentation/paciente_list/PacienteListViewModel.kt
@@ -1,13 +1,27 @@
 package com.example.micardioat.presentation.paciente_list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.micardioat.domain.model.PacienteCardiologia
 import com.example.micardioat.domain.use_case.GetPacientesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class PacienteListViewModel @Inject constructor(
-    private val getPacientesUseCase: GetPacientesUseCase
+    getPacientesUseCase: GetPacientesUseCase
 ) : ViewModel() {
-
+    val pacientes: StateFlow<List<PacienteCardiologia>> = getPacientesUseCase()
+        .map { resource ->
+            resource.data ?: emptyList()
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = emptyList()
+        )
 }
