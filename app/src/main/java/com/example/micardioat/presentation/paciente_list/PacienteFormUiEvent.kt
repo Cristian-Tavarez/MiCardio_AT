6578a8e284ec9f9ce2d3a69f6a90235e32d9a3bd
Cristian-Tavarez <cristian22041@gmail.com>
2026-08-17 13:48:30 -0700

    Crea PacienteFormUiEvent.kt

---
+++ app/src/main/java/com/example/micardioat/presentation/paciente_list/PacienteFormUiEvent.kt
@@ -0,0 +1,28 @@
+package com.example.micardioat.presentation.paciente_list
+
+sealed interface PacienteFormUiEvent {
+    data class Load(val id: Int?) : PacienteFormUiEvent
+
+    data class NombreChanged(val value: String) : PacienteFormUiEvent
+    data class EdadChanged(val value: String) : PacienteFormUiEvent
+    data class DiagnosticoChanged(val value: String) : PacienteFormUiEvent
+    data class PresionArterialChanged(val value: String) : PacienteFormUiEvent
+    data class SexoChanged(val value: String) : PacienteFormUiEvent
+    data class MotivoConsultaChanged(val value: String) : PacienteFormUiEvent
+    data class FcChanged(val value: String) : PacienteFormUiEvent
+    data class FrChanged(val value: String) : PacienteFormUiEvent
+    data class AntecedentesQuirurgicosChanged(val value: String) : PacienteFormUiEvent
+    data class AntecedentesPatologicosChanged(val value: String) : PacienteFormUiEvent
+    data class TratamientoChanged(val value: String) : PacienteFormUiEvent
+    data class AlergiasChanged(val value: String) : PacienteFormUiEvent
+    data class HbChanged(val value: String) : PacienteFormUiEvent
+    data class HctChanged(val value: String) : PacienteFormUiEvent
+    data class GlicemiaChanged(val value: String) : PacienteFormUiEvent
+    data class ColTotalChanged(val value: String) : PacienteFormUiEvent
+    data class FeviChanged(val value: String) : PacienteFormUiEvent
+    data class PlanChanged(val value: String) : PacienteFormUiEvent
+    data class FechaCitaChanged(val value: Long?) : PacienteFormUiEvent
+
+    data object Save : PacienteFormUiEvent
+    data object Delete : PacienteFormUiEvent
+}
