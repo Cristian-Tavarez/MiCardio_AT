e3502541169ab7a7aa2616edaa709e191a02d053
Heyson Polanco <Heysonpolanco65@gmail.com>
2026-08-18 15:03:11 -0400

    Added new history tables to the app's local database.

--- app/src/main/java/com/example/micardioat/domain/use_case/GetPacienteByIdUseCase.kt
+++ app/src/main/java/com/example/micardioat/domain/use_case/GetPacienteByIdUseCase.kt
@@ -1,7 +1,7 @@
 package com.example.micardioat.domain.use_case
 
 import com.example.micardioat.data.repository.PacienteCardiologiaRepository
-import com.example.micardioat.domain.model.PacienteCardiologia
+import com.example.micardioat.domain.model.PacienteDetalle
 import com.example.micardioat.utils.Resource
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -9,7 +9,7 @@ import javax.inject.Inject
 class GetPacienteByIdUseCase @Inject constructor(
     private val repository: PacienteCardiologiaRepository
 ) {
-    operator fun invoke(id: Int): Flow<Resource<PacienteCardiologia>> {
+    operator fun invoke(id: Int): Flow<Resource<PacienteDetalle>> {
         return repository.getPacienteById(id)
     }
 }
