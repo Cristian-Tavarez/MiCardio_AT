8716d0ef8418aaa4c5cccfbc91c0a9ac1ffcbf4c
Heyson Polanco <Heysonpolanco65@gmail.com>
2026-08-18 15:17:57 -0400

    removed unused menuAnchor

--- app/src/main/java/com/example/micardioat/presentation/paciente_list/PacienteListScreen.kt
+++ app/src/main/java/com/example/micardioat/presentation/paciente_list/PacienteListScreen.kt
@@ -19,7 +19,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -283,7 +282,7 @@ fun QuickScheduleDialog(
                             focusedBorderColor = accentTeal,
                             focusedLabelColor = accentTeal
                         ),
-                        modifier = Modifier.menuAnchor().fillMaxWidth()
+                        modifier = Modifier.fillMaxWidth()
                     )
                     ExposedDropdownMenu(
                         expanded = expanded,
